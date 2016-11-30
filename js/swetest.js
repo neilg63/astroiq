a1eaf660178ab0c377bbb407b52b2a12d0ba15fd
Neil Gardner <neilg@qpc-H97-D3H>
2016-11-30 19:15:59 +0000

    added command line test utility

---
+++ js/swetest.js
@@ -0,0 +1,28 @@
+(function($) {
+    $( document ).ready(function() {
+        var cf = $('form#control-form');
+
+        cf.on('submit',function(e){
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            var cmdEl =$('#form-cmd'),cmd;
+            if (cmdEl.length>0) {
+                cmd = cmdEl.val();
+                var params = {
+                    cmd: cmd
+                }
+                var href = '/swetest-backend';
+                $.ajax({
+                    url: href,
+                    data: params,
+                    success: function(data) {
+                        if (data.valid) {
+                            $('#results-pane pre').html(data.output);
+                        }
+                    }
+                });
+
+            }
+        });
+    });
+})(jQuery);
