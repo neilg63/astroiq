7892f4a4c2350dc33e497824f5d98fd70f19a19c
Neil Gardner <neilg@qpc-H97-D3H>
2016-12-13 19:04:16 +0000

    more work

--- js/chart.js
+++ js/chart.js
@@ -1,16 +1,16 @@
-(function($) {
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('gmap'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+}
 
-    function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('gmap'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
+(function($) {
 
     $( document ).ready(function() {
         var astroDisc = {
