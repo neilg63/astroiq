19b4aa4e2a972194da22a4051539c6aa8f2f4f46
Neil Gardner <neilgardner1963@gmail.com>
2016-12-17 23:40:28 +0000

    more work on map

--- js/chart.js
+++ js/chart.js
@@ -195,7 +195,7 @@ function initMap() {
                 if (valid) {
                     var m = new Snap.Matrix();
                     m.rotate(newBounds[0],r+this.offset,r+this.offset);
-                    m.translate(0-this.offset,0-this.offset);
+                    m.translate(this.offset,this.offset);
                     this.central.animate({
                         transform: m
                     },500,mina.easein);
