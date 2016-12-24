0b1134d04583eaa6e0baa0a3a7c8bb8f7d254881
Neil Gardner <neilgardner1963@gmail.com>
2016-12-24 01:02:34 +0000

    integrated font and experimental use of pug library

--- js/chart.js
+++ js/chart.js
@@ -229,7 +229,7 @@ function initMap() {
                     seg = this.addSegment(spanDeg,startDeg,this.colors[i%this.colors.length],i);
                     this.central.append(seg);
                     this.segments.push(seg);    
-                    var pos = this.calcCircPos(hb[i],(r + this.offset),-10,10);
+                    var pos = this.calcCircPos(hb[i],(r * 0.75),-10,10);
                     this.snap.text(pos.x,pos.y,(i+1).toString()).attr({
                         'class': 'house-label'
                     });
